493526f3fc6f6aa788f1d538e1bb0f9b3c642094
messi00100 <60361773+messi00100@users.noreply.github.com>
2021-01-04 23:23:14 +0800

    first commit for variable file

---
+++ variable.tf
@@ -0,0 +1,15 @@
+variable "access_key" {
+  default = "xxxxxxxxxxxxxxx"
+}
+
+variable "secret_key" {
+  default  = "xxxxxxxxxxxxxxx"
+}
+
+variable "region" {
+  default = "cn-beijing"
+}
+
+variable "password" {
+  default = "xxxxxxxxxxxxxxx"
+}
